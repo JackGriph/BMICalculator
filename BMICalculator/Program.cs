d17ee86d5c579674e5b01a0a0559b8e9b6ea9072
Jack Griph <jackgriph111@hotmail.com>
2025-09-15 14:20:45 +0200

    Add project files.

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Formats.Asn1;
+
+namespace BMICalculator
+{
+    internal class Program
+    {
+        static double CalculateBMI(double weight, double height, string unit = "metric")
+        {
+            if ( unit == "metric"  )
+            {
+                return weight / (height * height);
+            }
+            else if ( unit == "imperial" )
+            {
+                return (weight / (height * height)) * 703;
+            }
+            else
+            {
+                Console.WriteLine("Okänd enhet, returnerar 0");
+                return 0;
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            // Vanligt anrop (metric, default)
+            double bmi1 = CalculateBMI(70, 1.78);
+            Console.WriteLine($"BMI (metric, default): {bmi1:F2}");
+
+            // Namngivna i ordning
+            double bmi2 = CalculateBMI(height: 1.78, weight: 70);
+
+        }
+    }
+}
